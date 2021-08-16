d7931ee4f2e460caf13e6bae0c56e58007d1f125
FallenAngel97 <mr.x97@mail.ru>
2021-08-16 09:59:44 +0300

    Initial commit

---
+++ app/app.template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> <?= $title ?> </title>
+		<meta name="keywords" content="<?= $keywords ?>" />
+		<meta name="description" content="<?= $description ?>" />
+		<!-- Default Stylings -->
+		<link rel="stylesheet" type="text/css" href="assets/style.css" />
+		<script src="assets/script.js"></script>
+	</head>
+	<body>
+		<?= $content ?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$title = "PCS - Pure PHP CMS";
+$keywords = "CMS, PHP, management system";
+$description = "Ecosystem, which tries to make the configuration of your files as minimum, as it can be";
+
+require_once("./app/app.template.php");
+
